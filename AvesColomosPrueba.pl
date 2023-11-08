8e69e71e87ae7ca2017512a87239103dd4939e0b
Ordna-dev <ordnajelaorerreug@gmail.com>
2023-11-08 14:51:30 -0600

    Por ultimo falta la identificacion de aves

--- AvesColomosPrueba.pl
+++ AvesColomosPrueba.pl
@@ -3,3 +3,8 @@ ave(garceta_pie_dorado, amarillo, amarillo, negro, blanco, blanco, negro, amaril
 ave(garceta_pie_dorado_joven, amarillo, amarillo, [negro, 'amarillo con la punta negra'], blanco, ['amarillos', 'verde amarillento'], amarillo, null , null, blanco, blanco, acuatico, [peces, insectos, crustaceos]).
 
 ave(garza_ganadera_adulto, amarillo, ['amarillos', anaranjado], blanco, negras, negros, ['anaranjado claro'],null ,null , ['anaranjado claro'], ['anaranjado claro'], [granjas, sembradios, 'cuerpos de agua'], [insectos, 'huevos de otras aves', peces]).
+ave(xdddd, a, b, c, d, e, f, g, h, i, j, k, l).
+ave(prueba2, [rojo,azul], null, null, null, null, null, [a], null, null, null, [b], null).
+ave(prueba3, null, null, null, null, [ehe,jj], null, null, [a], null, null, null, [k,l]).
+ave(agmmmm, [agm], null, null, null, null, null, null, null, null, null, null, [x]).
+ave(opakopd, null, null, null, null, null, [retyy], null, null, [ertyt], null, null, null).
