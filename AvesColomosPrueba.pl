2af232e1cc8573478c9b0dd8dbfa8c03052f00fd
Ordna-dev <ordnajelaorerreug@gmail.com>
2023-11-08 12:42:20 -0600

    Ya lee el contenido del archivo, haciendo scrollbar para muestra

---
+++ AvesColomosPrueba.pl
@@ -0,0 +1,5 @@
+ave(garceta_pie_dorado, amarillo, amarillo, negro, blanco, blanco, negro, amarillo, blanco, blanco, null , acuatico, [peces, insectos, crustaceos]).
+
+ave(garceta_pie_dorado_joven, amarillo, amarillo, [negro, 'amarillo con la punta negra'], blanco, ['amarillos', 'verde amarillento'], amarillo, null , null, blanco, blanco, acuatico, [peces, insectos, crustaceos]).
+
+ave(garza_ganadera_adulto, amarillo, ['amarillos', anaranjado], blanco, negras, negros, ['anaranjado claro'],null ,null , ['anaranjado claro'], ['anaranjado claro'], [granjas, sembradios, 'cuerpos de agua'], [insectos, 'huevos de otras aves', peces]).
